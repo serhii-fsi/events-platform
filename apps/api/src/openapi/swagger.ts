23a50c5b9b7251d211f4d233265fe1270a42edcc
Serhii FSI <fsitmp@gmail.com>
2024-12-15 04:20:33 +0000

    Add OpenAPI TypeScript types generation

--- apps/api/src/openapi/swagger.ts
+++ apps/api/src/openapi/swagger.ts
@@ -6,7 +6,7 @@ export function setupSwagger(app: Express) {
   const swaggerDocument = YAML.load(
     // We need to use dist/apps/api to get the correct path because the app is running
     // from the dist folder and typscript compiles the code to the dist folder except yaml file
-    __dirname.split('dist/apps/api')[0] + 'apps/api/src/openapi/swagger.yaml'
+    __dirname.split('dist/apps/api')[0] + 'apps/api/src/openapi/schema.yaml'
   );
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 }
