6da8d8175307d25bd51a2022094f6d022f5d0793
Serhii FSI <fsitmp@gmail.com>
2024-12-15 03:18:46 +0000

    Add Swagger UI integration and update API routes; include new dependencies

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -17,55 +17,12 @@ const port = Number(process.env.PORT);
 
 const app = express();
 
-// //--------- test
-// import { Pool } from 'pg';
-// import { drizzle } from 'drizzle-orm/node-postgres';
-// import { serial, varchar, pgTable } from 'drizzle-orm/pg-core';
-// import { sql } from 'drizzle-orm';
-
-// // Define the users table schema
-// const users = pgTable('users', {
-//   id: serial('id').primaryKey(),
-//   name: varchar('name', { length: 100 }).notNull(),
-// });
-
-// const pool = new Pool({
-//   user: process.env.DB_USER,
-//   host: process.env.DB_HOST,
-//   database: process.env.DB_DB,
-//   password: process.env.DB_PASSWORD,
-//   port: Number(process.env.DB_PORT),
-// });
-
-// const db = drizzle(pool);
-
-// // Create table and insert data
-// const setupDatabase = async () => {
-//   await db.execute(sql`
-//     CREATE TABLE IF NOT EXISTS users (
-//       id SERIAL PRIMARY KEY,
-//       name VARCHAR(100) NOT NULL
-//     )
-//   `);
-
-//   await db.insert(users).values([{ name: 'John Doe' }, { name: 'Jane Doe' }]);
-// };
-
-// setupDatabase();
-
-// app.get('/users', async (req, res) => {
-//   try {
-//     const allUsers = await db.select().from(users);
-//     res.json({ message: 'Hello111', allUsers: allUsers });
-//   } catch (err) {
-//     res.status(500).json({ error: err.message });
-//   }
-// });
-// //--------- test
+import { setupSwagger } from './openapi/swagger';
+setupSwagger(app);
 
 // Create tables if they don't exist and seed db
 setupDb();
-//
+
 // Seed db
 if (DEV_MODE || TEST_MODE)
   app.get('/seed-db', async (req, res) => {
@@ -90,7 +47,7 @@ if (DEV_MODE || TEST_MODE)
     }
   });
 
-app.get('/users', async (req, res) => {
+app.get('/api/users', async (req, res) => {
   await db.update(users).set({ name: 'John Doe' }).where(eq(users.id, 1));
 
   const allUsers = await db.select().from(users);
---
+++ apps/api/src/openapi/swagger.ts
@@ -0,0 +1,12 @@
+import swaggerUi from 'swagger-ui-express';
+import * as YAML from 'yamljs';
+import { Express } from 'express';
+
+export function setupSwagger(app: Express) {
+  const swaggerDocument = YAML.load(
+    // We need to use dist/apps/api to get the correct path because the app is running
+    // from the dist folder and typscript compiles the code to the dist folder except yaml file
+    __dirname.split('dist/apps/api')[0] + 'apps/api/src/openapi/swagger.yaml'
+  );
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+}
